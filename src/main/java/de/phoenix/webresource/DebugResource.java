f2429ca70e3ef6cb9ae5a802ee6499c4cbaacd18
Kilian Gaertner <Bhaals@gmx.de>
2014-03-14 16:16:10 +0100

    Add debug resurce

---
+++ src/main/java/de/phoenix/webresource/DebugResource.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2014 Project-Phoenix
+ * 
+ * This file is part of WebService.
+ * 
+ * WebService is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * WebService is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with WebService.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.phoenix.webresource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("debug")
+public class DebugResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getLog() {
+
+        return Response.ok("Debug information\nOneLine\nSecondLine").build();
+    }
+}
---
+++ src/test/java/de/phoenix/webresource/DebugTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2014 Project-Phoenix
+ * 
+ * This file is part of WebService.
+ * 
+ * WebService is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * WebService is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with WebService.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.phoenix.webresource;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.test.framework.AppDescriptor;
+import com.sun.jersey.test.framework.JerseyTest;
+import com.sun.jersey.test.framework.WebAppDescriptor;
+
+import de.phoenix.junit.OrderedRunner;
+import de.phoenix.junit.OrderedRunner.Order;
+
+@RunWith(OrderedRunner.class)
+public class DebugTest extends JerseyTest {
+
+    @Override
+    protected AppDescriptor configure() {
+        return new WebAppDescriptor.Builder("de.phoenix").build();
+    }
+
+    @Test
+    @Order(1)
+    public void testDebug() {
+        WebResource web = resource().path("debug");
+        String res = web.get(String.class);
+        assertEquals("Debug information\nOneLine\nSecondLine", res);
+    }
+}
